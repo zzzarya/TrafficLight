8085d4f8a8b4ee180b1a68fb09deff2e4b803af4
Anton Zarichny <antonzarichny@gmail.com>
2022-07-29 19:18:22 +0300

    1. New settings for views and button

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.layer.cornerRadius = redLightView.frame.size.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.size.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.size.width / 2
+        
+        startButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func startButtonPressed() {
+    }
+    
 }
 
